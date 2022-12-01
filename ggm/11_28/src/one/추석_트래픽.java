39a5b6393e3c7efc0a5b8d6cb8eb4b1f56948aa5
hi050227 <ho050227@naver.com>
2022-12-01 16:29:45 +0900

    trafic

--- ggm/11_28/src/one/추석_트래픽.java
+++ ggm/11_28/src/one/추석_트래픽.java
@@ -1,5 +1,7 @@
 package one;
 
+import java.util.Arrays;
+
 /*
  * 장애 대비용 서버 증설 여부 결정하기 위해 작년 추석 기간인 9월 15일 로그 데이터를 분석
  * 초당 최대 처리량을 계산해봄 1초간 처리하는 요청의 최대 개수
@@ -43,6 +45,59 @@ public class 추석_트래픽 {
 	}
 	public static int solution(String[] lines) {
         int answer = 0;
+        
+        
+        double[] processTimes = new double[lines.length];
+        double[][] times = new double[lines.length][2];
+        
+        for(int i = 0; i < lines.length; i++) {
+        	String[] line = lines[i].split(" ");
+        	
+        	String[] time = line[1].split(":");
+        	
+        	times[i][1] = Double.parseDouble(time[0]) * 3600 + Double.parseDouble(time[1]) * 60 + Double.parseDouble(time[2]);
+//        	System.out.println(Arrays.toString(time));
+//        	System.out.println(times[i][1]);
+//        	System.out.println(line[2]);
+        	
+        	processTimes[i] = Double.parseDouble(line[2].replaceAll("s", ""));
+//        	System.out.println(processTimes[i]);
+        	
+        	times[i][0] = Math.round((times[i][1] - processTimes[i] + 0.001) * 1000) / 1000.0;
+        	
+        	
+        	
+        }
+        
+        double minTime = times[0][0];
+        double maxTime = times[lines.length-1][1];
+        
+        
+        
+        int cnt = 0;
+        
+//        while(minTime <= maxTime) {
+//        	double endTime = minTime + 1.0;
+//        	cnt = 0;
+//        	
+//        	
+//        	for(int i = 0; i < lines.length; i++) {
+//        		if(minTime <= times[i][0] && endTime >= times[i][0]) {
+//        			cnt++;
+//        		}
+//        		else if(minTime <= times[i][1] && endTime >= times[i][1]) {
+//        			cnt++;
+//        		}
+//        		else if(minTime >= times[i][0] && endTime <= times[i][1]) {
+//        			cnt++;
+//        		}
+//        	}
+//        	
+//        	answer = Math.max(answer, cnt);
+////        	System.out.println(minTime + " " + endTime);
+//        	minTime += 0.001;
+//        }
+        
         return answer;
     }
 
